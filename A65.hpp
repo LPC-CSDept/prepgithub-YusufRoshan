1f28ef85cdfb3ef022fccb123bf43be65204d961
yusufroshan <yusuf.roshan04@gmail.com>
2022-10-10 21:39:21 -0700

    A65#1

---
+++ A65.hpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2, num3; 
+    int print; 
+
+    void getinput(int num1, int num2, int num3);
+    void swap(int num1, int num2, int num3); 
+    print swap; 
+
+}
